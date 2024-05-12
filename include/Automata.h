fc47c98bf331647062e792f14c9c72a0ccbdd54c
Kirill Siryakov <121825657+Siryakov@users.noreply.github.com>
2024-05-12 20:39:52 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -24,26 +24,26 @@ struct Drink {
 };
 
 class Automata {
-    private:
-        int cash;
-        std::vector<Drink> menu;
-        std::vector<int> prices;
-        STATES state;
+ private:
+    int cash;
+    std::vector<Drink> menu;
+    std::vector<int> prices;
+    STATES state;
 
-    public:
-        Automata();
-        void on(); //включение автомата;
-        void off(); //выключение автомата;
-        void coin(int amount); //занесение денег на счёт пользователем;
-        void getMenu(); //считывание меню с напитками и ценами для пользователя;
-        void getState(); //считывание текущего состояния для пользователя;
-        bool choice(int option); //выбор напитка пользователем;
-        bool check(int option); //проверка наличия необходимой суммы;
-        void cancel(); //отмена сеанса обслуживания пользователем;
-        void cook(int option); //имитация процесса приготовления напитка;
-        void finish(); //завершение обслуживания пользователя.
-        void error(const std::string& message);
-        void getBalance() const;
+ public:
+    Automata();
+    void on(); //включение автомата;
+    void off(); //выключение автомата;
+    void coin(int amount); //занесение денег на счёт пользователем;
+    void getMenu(); //считывание меню с напитками и ценами для пользователя;
+    void getState(); //считывание текущего состояния для пользователя;
+    bool choice(int option); //выбор напитка пользователем;
+    bool check(int option); //проверка наличия необходимой суммы;
+    void cancel(); //отмена сеанса обслуживания пользователем;
+    void cook(int option); //имитация процесса приготовления напитка;
+    void finish(); //завершение обслуживания пользователя.
+    void error(const std::string& message);
+    void getBalance() const;
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
