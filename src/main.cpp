e83103991734656a8f8e4b9f40ae27594819dfe5
Kirill Siryakov <121825657+Siryakov@users.noreply.github.com>
2024-05-12 19:44:00 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Automata.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Automata coffeeMachine;
+
+    // Включение автомата
+    coffeeMachine.on();
+
+    // Добавление монет
+    coffeeMachine.coin(50); 
+    coffeeMachine.coin(100); 
+    coffeeMachine.coin(200); 
+
+    // Получение и отображение полного меню
+    coffeeMachine.getMenu();
+
+    // Выбор напитка
+    coffeeMachine.choice(2); 
+
+    // Получение остатка на счету
+    coffeeMachine.getBalance();
+
+    // Отмена заказа
+    coffeeMachine.cancel();
+
+    // Выключение автомата
+    coffeeMachine.off();
+
+    return 0;
+}
